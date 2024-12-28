b0868532444479fd15a050a92b198ed4f931689d
tijareshubhangi <shubhangitijare01@gmail.com>
2024-12-28 16:25:45 +0530

    all code

--- Frontend/Backend/config/db.js
+++ Frontend/Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"; // Add this line
 const connectDB = async () => {
   try {
     // const res = await mongoose.connect("mongodb+srv://amodchaudhari94:PGcuFsuALos6FOmx@lms01.zc9bb.mongodb.net/");
-    const res = await mongoose.connect("mongodb+srv://hospitalsdaya:oWxEOShnqT1HhkjZ@suceesslms.bgcof.mongodb.net/lms");
+    const res = await mongoose.connect("mongodb+srv://noxadstrom110:tYQttlM5yvMpUzp2@new.xfimn.mongodb.net/?retryWrites=true&w=majority&appName=new");
     console.log("Database connected successfully");
   } catch (err) {
     console.error("Database connection error:", err);
